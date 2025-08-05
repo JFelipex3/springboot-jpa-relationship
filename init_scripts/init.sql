17e648d30475a6e9abcc1697ed6f479b8e8e0819
JFelipex3 <jfelipe.machuca@gmail.com>
2025-08-04 23:08:06 -0400

    Archivo solo referencia

--- init_scripts/init.sql
+++ init_scripts/init.sql
@@ -11,9 +11,25 @@ CREATE TABLE invoices (
   total bigint DEFAULT NULL,
   description varchar(255) DEFAULT NULL,
   PRIMARY KEY (id),
-  KEY FK_client_id (client_id),
-  CONSTRAINT FK_client_id FOREIGN KEY (client_id) REFERENCES clients (id)
+  KEY fk_client_id (client_id),
+  CONSTRAINT fk_client_id FOREIGN KEY (client_id) REFERENCES clients (id)
 );
+
+CREATE TABLE addresses (
+  id bigint NOT NULL AUTO_INCREMENT,
+  street varchar(255) DEFAULT NULL,
+  number int DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE clients_addresses (
+  addresses_id bigint NOT NULL,
+  client_id bigint NOT NULL,
+  UNIQUE KEY pk_client_addresses_id (addresses_id),
+  KEY fk_client_id (client_id),
+  CONSTRAINT fk_client_id FOREIGN KEY (client_id) REFERENCES clients (id),
+  CONSTRAINT fk_addresses_id FOREIGN KEY (addresses_id) REFERENCES addresses (id)
+)
 
 insert into clients (name, lastname) values ('Pepe', 'Doe');
 insert into clients (name, lastname) values ('Maria', 'Roe');
