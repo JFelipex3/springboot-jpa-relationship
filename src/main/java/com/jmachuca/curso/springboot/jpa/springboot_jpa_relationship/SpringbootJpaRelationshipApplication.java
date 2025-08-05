101e0b5a8e6ae18c4bf5e51933742d7703795f99
JFelipex3 <jfelipe.machuca@gmail.com>
2025-08-04 22:09:34 -0400

    ManyToOne con cliente ya existente

--- src/main/java/com/jmachuca/curso/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
+++ src/main/java/com/jmachuca/curso/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
@@ -1,6 +1,9 @@
 package com.jmachuca.curso.springboot.jpa.springboot_jpa_relationship;
 
 import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -30,10 +33,10 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		manyToOne();
+		manyToOneFindByIdClient(1L);
 	}
 
-	public void manyToOne() {
+	public void manyToOneCreateClient() {
 
 		Client client = new Client("John", "Doe");
 		clientRepository.save(client);
@@ -43,6 +46,22 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner{
 		Invoice invoiceDB = invoiceRepository.save(invoice);
 
 		System.out.println(invoiceDB);
+	}
+
+	public void manyToOneFindByIdClient(Long clientId) {
+
+		Optional<Client> optionalClient = clientRepository.findById(clientId);
+
+		optionalClient.ifPresentOrElse(client -> {
+			Invoice invoice = new Invoice("Compras de Oficina", 2000L);
+			invoice.setClient(client);
+			Invoice invoiceDB = invoiceRepository.save(invoice);
+
+			System.out.println(invoiceDB);
+
+		}, () -> {
+			System.out.println("El cliente no existe");
+		});
 	}
 
 }
